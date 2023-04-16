294ca0f6d79167153743808ca3237e6740607dbc
unknown <lakshitgurtatta@gmail.com>
2023-04-16 10:41:08 +0530

    Added Testng

---
+++ src/main/java/testcases/CreditCardTest.java
@@ -0,0 +1,14 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class CreditCardTest extends BaseClass {
+	
+	@Test(groups= {"Sanity"})
+	public void test4() {
+		System.out.println("inside CC test4");
+	}
+	
+	
+
+}
